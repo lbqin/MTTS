242889536c37faeceb18e415c3d3cf29dfb45e87
lbq <baoqin.luo@avatarworks.com>
2018-08-09 15:02:14 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+voice=dabai_48k
+data_dir=/home/research/data1/MTTS_data
+voice_path=${data_dir}/${voice}
+
+python3 src/mtts.py ${voice_path}/fullData.txt ${voice_path}/wav ${voice_path}/output
+
